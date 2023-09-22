77bdd06b962903a176bf53a2d8c04b9f5a92cf8e
Sadjoaldi <alsannealdi@outlook.com>
2023-09-22 06:52:19 +0200

    debut todo

--- to_doL.js
+++ to_doL.js
@@ -1,8 +1,45 @@
-const form = document.querySelector("form");
+// const form = document.querySelector("form");
 //storage
-function storeList() {
+// function storeList() {
+//   window.localStorage.todolist = list.innerHTML;
+// }
+
+// function getTodos() {
+//   if (window.localStorage.todolist) {
+//     list.innerHTML = window.localStorage.todolist;
+//   } else {
+//     list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+//   }
+// }
+// window.addEventListener("load", getTodos);
+
+//add element
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault(); //permet de se premnunir contre son comportement par default(type rechager la page)
+//   list.innerHTML += `<li>${item.value}</li>`;
+//   item.value = "";
+//   storeList();
+// });
+
+//remove element
+// list.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("checked")) {
+//     e.target.remove();
+//   } else {
+//     e.target.classList.add("checked");
+//   }
+//   storeList();
+// });
+
+// *****
+
+// *on commence par selectionner le formulaire**
+const form = document.querySelector("form");
+//*faire une fonction de stockage
+function storageList() {
   window.localStorage.todolist = list.innerHTML;
 }
+//*function pour attribuer la valeur du local storage à ma list
 
 function getTodos() {
   if (window.localStorage.todolist) {
@@ -11,24 +48,3 @@ function getTodos() {
     list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
   }
 }
-window.addEventListener("load", getTodos);
-
-//add element
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); //permet de se premnunir contre son comportement par default(type rechager la page)
-  list.innerHTML += `<li>${item.value}</li>`;
-  item.value = "";
-  storeList();
-});
-
-//remove element
-list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("checked")) {
-    e.target.remove();
-  } else {
-    e.target.classList.add("checked");
-  }
-  storeList();
-});
-
-//*****
